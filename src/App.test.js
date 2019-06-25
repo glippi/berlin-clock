32297f441fac018893ef5af521758570d1961799
Gabriele Lippi <gabriele@lippi.net>
2019-06-25 17:58:41 +0200

    Writing failing test for BerlinClock

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App should', () => {
+  test('render BerlinClock', () => {
+    const { getByTestId } = render(<App />);
+
+    expect(getByTestId('berlin-clock')).toBeTruthy();
+  });
 });
