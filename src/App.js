aff35abc44ffed0827a5419f219ea097fd834670
Gabriele Lippi <gabriele@lippi.net>
2019-06-27 11:32:33 +0200

    Create StandardClock component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import { BerlinClock } from './BerlinClock';
-import { parseDate } from './clockEngine';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import { BerlinClock } from './BerlinClock'
+import { StandardClock } from './StandardClock'
+import { parseDate } from './clockEngine'
 
 function startClock() {
-  const today = new Date();
-  const h = today.getHours();
-  const m = today.getMinutes() < 10 ? `0${today.getMinutes()}` : today.getMinutes();
-  const s = today.getSeconds() < 10 ? `0${today.getSeconds()}` : today.getSeconds();
-  return `${h}:${m}:${s}`;
+  const today = new Date()
+  const h = today.getHours()
+  const m =
+    today.getMinutes() < 10 ? `0${today.getMinutes()}` : today.getMinutes()
+  const s =
+    today.getSeconds() < 10 ? `0${today.getSeconds()}` : today.getSeconds()
+  return `${h}:${m}:${s}`
 }
 
 function App() {
-  const [ time, setTime ] = useState('');
+  const [time, setTime] = useState('')
 
   useEffect(() => {
     setInterval(() => {
-      setTime(startClock());
-    }, 1000);
-  });
+      setTime(startClock())
+    }, 1000)
+  })
 
-  const fromLocaleToBerlinFormat = parseDate(time);
+  const fromLocaleToBerlinFormat = parseDate(time)
 
   return (
     <main>
@@ -31,19 +34,20 @@ function App() {
             className="App-link"
             href="https://en.wikipedia.org/wiki/Mengenlehreuhr"
             target="_blank"
-            rel="noopener noreferrer">
-            {' '}Berlin Clock{' '}
+            rel="noopener noreferrer"
+          >
+            {' '}
+            Berlin Clock{' '}
           </a>
         </h1>
       </header>
 
       <section className="flex flex-row justify-around items-center">
-        {time}
+        <StandardClock time={time} />
         <BerlinClock time={fromLocaleToBerlinFormat} />
       </section>
-
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/StandardClock.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function StandardClock({ time }) {
+  return <div data-testid="standard-clock">{time}</div>
+}
