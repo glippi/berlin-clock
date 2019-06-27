d5878da6d992506cc9b8e9068134f65a26d0fbb1
Gabriele Lippi <gabriele@lippi.net>
2019-06-27 11:10:24 +0200

    Add failing test for StandardClock

--- src/App.test.js
+++ src/App.test.js
@@ -8,4 +8,10 @@ describe('App should', () => {
 
     expect(getByTestId('berlin-clock')).toBeTruthy();
   });
+
+  test('render StandardClock', () => {
+    const { getByTestId } = render(<App />);
+
+    expect(getByTestId('standard-clock')).toBeTruthy();
+  });
 });
