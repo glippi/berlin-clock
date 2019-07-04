acc2ffa9bf2d76011d3e632c5079efd0b72daaa0
Gabriele Lippi <gabriele@lippi.net>
2019-07-04 16:35:16 +0200

    Fix failing test

--- src/startClock.test.js
+++ src/startClock.test.js
@@ -6,6 +6,7 @@ describe('startClock should', () => {
   test('give the actual time in hh:mm:ss format', () => {
     const MOCKED_DATE = new Date('2019-05-14T11:01:08.135Z')
     global.Date = jest.fn(() => MOCKED_DATE)
+
     const formattedDate = startClock(new Date())
 
     expect(formattedDate).toBe('13:01:08')
